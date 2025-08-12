3c208730719d17b280776eadb54310ebd1b061c6
bunchuq1122 <hobul1004@gmail.com>
2025-08-12 20:53:50 +0900

    !fuckrrozy fixed again

--- src/index.ts
+++ src/index.ts
@@ -669,7 +669,7 @@ client.on('messageCreate', async (message: Message) => {
     return;
   }
   if (message.channel.isTextBased()) {
-    await (message.channel as TextChannel).send(roleMention('rrozy') + 'got fucked by' + member.nickname);
+    await (message.channel as TextChannel).send(roleMention(process.env.RROZY || 'rrozy')+ ' ' + 'got fucked by' + ' '+ member);
   }
 });
 
